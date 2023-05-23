0e9edbc36a3abc85149c58813d94e00016609339
lucasafonsobastos <lucasafonsob@hotmail.com>
2023-05-22 22:47:16 -0300

    mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,12 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold
+}  from '@expo-google-fonts/roboto';
+
+import { BaiJamjuree_700Bold }  from '@expo-google-fonts/bai-jamjuree';
+
 export default function App() {
+  
+  const [hasLoadFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  if(!hasLoadFonts) {
+    return null;
+  }
+
   return (
-    <View className="flex-1 justify-center items-center bg-gray-950">
-      <Text>Hello Word - testando </Text>
-      <StatusBar style="auto" />
+    <View className="flex-1 items-center justify-center bg-gray-950 ">
+      <Text className="font-alt text-5xl text-gray-50" >Testando!</Text>
+      <StatusBar style="light" />
     </View>
   );
 }
-
--- mobile/tailwind.config.js
+++ mobile/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./App.tsx', './app/**/*.tsx'],
+  content: ["./App.{js,jsx,ts,tsx}", "./<custom directory>/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       fontFamily: {
